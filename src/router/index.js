99af53aa404a16ef467d7457775fc24b90c84120
CyberShriver <irakozep18.pi@gmail.com>
2023-02-19 19:43:24 +0200

    add [home,about] in router and it's meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/Home.vue";
+import AboutView from "../views/About.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ ],
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: HomeView,
+      meta: {
+        title: "Home",
+      },
+    },
+    {
+      path: "/About",
+      name: "About",
+      component: AboutView,
+      meta: {
+        title: "About",
+      },
+    },
+  ],
 });
 
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title } | Side Bar`;
+  next();
+});
 
 export default router;
