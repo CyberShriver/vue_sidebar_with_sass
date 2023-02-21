5cff2da0b07c53fa5f8acd093e2156e99a314b79
CyberShriver <irakozep18.pi@gmail.com>
2023-02-21 07:29:41 +0200

    add store in project

---
+++ src/components/wrapModal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col fixed">
+        <div class="form">
+            <div>
+              <input type="text" placeholder="Name" class="border-b"/>
+            </div>
+            <div>
+              <input type="text" placeholder="surname" class="border-b"/>
+            </div>
+            <div>
+              <input type="text" placeholder="age" class="border-b"/>
+            </div>
+            <div>
+              <input type="text" placeholder="place" class="border-b"/>
+            </div>
+            <div>
+              <input type="text" placeholder="sex" class="border-b"/>
+            </div>
+            <div>
+              <input type="text" placeholder="old" class="border-b"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
